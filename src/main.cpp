f9b921d36f9f12c876daf22c7fd9f008b71debba
misiek1999 <michaldydo99@gmail.com>
2024-01-31 23:26:34 +0100

    Code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,158 +1,30 @@
 #include <Arduino.h>
-#include <btstack.h>
-#include <Bluepad32.h>
-#include <btstack_port_esp32.h>
-extern "C" {
-#include <uni_esp32.h>
-#include <uni_property.h>
-#include <uni_bt_setup.h>
-#include <uni_bt_allowlist.h>
-#include <uni_virtual_device.h>
-}
 #include <nRF24L01.h>
+#include "Bluepad32_initialization.h"
 
-ControllerPtr myControllers[BP32_MAX_GAMEPADS];
-
-bool checkControlerHasValidAddress(ControllerPtr ctl) {
-  ControllerProperties properties = ctl->getProperties();
-  if (properties.btaddr[0] == 0 && properties.btaddr[1] == 0 &&
-      properties.btaddr[2] == 0 && properties.btaddr[3] == 0 &&
-      properties.btaddr[4] == 0 && properties.btaddr[5] == 0) {
-    return false;
-  }
-  return true;
-}
-// This callback gets called any time a new gamepad is connected.
-// Up to 4 gamepads can be connected at the same time.
-void onConnectedController(ControllerPtr ctl) {
-  bool foundEmptySlot = false;
-  for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
-    if (myControllers[i] == nullptr) {
-      if (checkControlerHasValidAddress(ctl) == false) {
-        return; // Ignore invalid controller
-      }
-      Serial.print("CALLBACK: Controller is connected, index=");
-      Serial.println(i);
-      myControllers[i] = ctl;
-      foundEmptySlot = true;
-
-      // Optional, once the gamepad is connected, request further info about the
-      // gamepad.
-      ControllerProperties properties = ctl->getProperties();
-      char buf[80];
-      sprintf(buf,
-              "BTAddr: %02x:%02x:%02x:%02x:%02x:%02x, VID/PID: %04x:%04x, "
-              "flags: 0x%02x",
-              properties.btaddr[0], properties.btaddr[1], properties.btaddr[2],
-              properties.btaddr[3], properties.btaddr[4], properties.btaddr[5],
-              properties.vendor_id, properties.product_id, properties.flags);
-      Serial.println(buf);
-      break;
-    }
-  }
-  if (!foundEmptySlot) {
-    Serial.println(
-        "CALLBACK: Controller connected, but could not found empty slot");
-  }
-}
-
-void onDisconnectedController(ControllerPtr ctl) {
-  bool foundGamepad = false;
-
-  for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
-    if (myControllers[i] == ctl) {
-      Serial.print("CALLBACK: Controller is disconnected from index=");
-      Serial.println(i);
-      myControllers[i] = nullptr;
-      foundGamepad = true;
-      break;
-    }
-  }
-
-  if (!foundGamepad) {
-    Serial.println(
-        "CALLBACK: Controller disconnected, but not found in myGamepads");
-  }
-}
-
-
-TaskHandle_t Task1;
-void wrapper_btstack_run_loop_execute( void * pvParameters ){
-  Serial.printf("Core ID BT: %d\n", xPortGetCoreID());
-  //   // Init btstack
-  printf("Init btstack\n");
-  btstack_init();
-
-  // Init bluepad32
-  Serial.printf("Start initialization bluepad32\n");
-  uni_property_init();
-  uni_platform_init(0, NULL);
-  uni_hid_device_setup();
-  // Continue with bluetooth setup.
-  uni_bt_setup();
-  uni_bt_allowlist_init();
-  uni_virtual_device_init();
-  Serial.printf("Init bluepad32 done\n");
-  Serial.printf("Bluepad32 enter to btstack run loop\n");
-  btstack_run_loop_execute();
-  Serial.printf("Illegal Bluepad32 exit from btstack run loop\n");
-
-}
+using namespace BP32Controller;
 
 // Arduino setup function. Runs in CPU 1
 void setup() {
-  Serial.begin(115200);
-  btStop();
-  // esp_log_level_set("*", ESP_LOG_VERBOSE);
-  // Read current core ID
-  Serial.printf("Core ID Arduino: %d\n", xPortGetCoreID());
+    // start serial port with baud rate 115200
+    Serial.begin(115200);
 
-  // // run btstack loop on second core
-  xTaskCreatePinnedToCore(
-                    wrapper_btstack_run_loop_execute,   /* Task function. */
-                    "btstack_run_loop_execute",     /* name of task. */
-                    20000,       /* Stack size of task */
-                    NULL,        /* parameter of the task */
-                    1,           /* priority of the task */
-                    &Task1,      /* Task handle to keep track of created task */
-                    0);          /* pin task to core 0 */
+    // esp_log_level_set("*", ESP_LOG_VERBOSE);
 
-  delay(2000);
+    // Read current core ID
+    Serial.printf("Core ID Arduino: %d\n", xPortGetCoreID());
 
-  // printf("Init btstack\n");
-  // btstack_init();
+    // Init bluepad32
+    init_bluepad32();
 
-  // // Init bluepad32
-  // Serial.printf("Start initialization bluepad32\n");
-  // uni_property_init();
-  // uni_platform_init(0, NULL);
-  // uni_hid_device_setup();
-  // // Continue with bluetooth setup.
-  // uni_bt_setup();
-  // uni_bt_allowlist_init();
-  // uni_virtual_device_init();
-  // Serial.printf("Init bluepad32 done\n");
-  // Serial.printf("Bluepad32 enter to btstack run loop\n");
-  // btstack_run_loop_execute();
-  // Serial.printf("Illegal Bluepad32 exit from btstack run loop\n");
+    // "forgetBluetoothKeys()" should be called when the user performs
+    // a "device factory reset", or similar.
+    // Calling "forgetBluetoothKeys" in setup() just as an example.
+    // Forgetting Bluetooth keys prevents "paired" gamepads to reconnect.
+    // But might also fix some connection / re-connection issues.
+    BP32.forgetBluetoothKeys();
 
-  // Init bluepad32
-  Serial.printf("Firmware: %s\n", BP32.firmwareVersion());
-  const uint8_t *addr = BP32.localBdAddress();
-  Serial.printf("BD Addr: %2X:%2X:%2X:%2X:%2X:%2X\n", addr[0], addr[1], addr[2],
-                addr[3], addr[4], addr[5]);
-
-  // Setup the Bluepad32 callbacks
-  BP32.setup(&onConnectedController, &onDisconnectedController);
-
-  // "forgetBluetoothKeys()" should be called when the user performs
-  // a "device factory reset", or similar.
-  // Calling "forgetBluetoothKeys" in setup() just as an example.
-  // Forgetting Bluetooth keys prevents "paired" gamepads to reconnect.
-  // But might also fix some connection / re-connection issues.
-  BP32.forgetBluetoothKeys();
-
-  Serial.println("Initialization successful!");
+    Serial.println("Initialization successful!");
 }
 
 
